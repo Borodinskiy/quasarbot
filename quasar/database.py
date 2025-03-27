eda157e3fc7d1aa64cf00f962a9ecd4291544a06
Borodinskiy <ivanforest856@gmail.com>
2025-03-28 00:34:49 +0300

    Так называемая база данных

--- quasar/database.py
+++ quasar/database.py
@@ -3,12 +3,21 @@ import sqlite3
 class DataBase():
 	"""Система хранения базы данных пользователей"""
 	def __init__(self, file):
-		self.database = ""
-
+		self.file = file
 		self.dbinit(file)
 
 	def dbinit(self, file):
-		self.database = sqlite3.connect(file)
+		self.file = file
+
+		database = sqlite3.connect(file)
+		cursor = database.cursor()
+
+		# Создание базы данных, если её до этого не было
+		cur.execute("CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), stage varchar(50))")
+		database.commit()
+
+		cur.close()
+		database.close()
 
 	def store_message(message):
 		pass
