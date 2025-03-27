b1ed427aac3fa8cfff57c87518ae614a813d6b30
Borodinskiy <ivanforest856@gmail.com>
2025-03-28 01:32:36 +0300

    Пример работы с моделью в ai_module

--- quasar/ai_module.py
+++ quasar/ai_module.py
@@ -1,14 +1,12 @@
-# import openai
-# from config import OPENAI_API_KEY
-#
-# openai.api_key = OPENAI_API_KEY
-# #неважно как не важно где
-# def get_ai_response(user_message):
-#     try:
-#         response = openai.ChatCompletion.create(
-#             model="gpt-4",
-#             messages=[{"role": "user", "content": user_message}]
-#         )
-#         return response["choices"][0]["message"]["content"]
-#     except Exception as e:
-#         return "Извините, я пока не могу ответить на этот вопрос."
+import lmstudio as lms
+
+model = lms.llm("llama-3.2-1b-instruct")
+
+# Неважно как не важно где
+def get_ai_response(user_message):
+	try:
+		return model.respond(user_message)
+	except Exception as e:
+		return "Извините, я пока не могу ответить на этот вопрос."
+
+print(model.respond("What a alex? KYM"))
